ec227c157a632b0827c77aba634c1d0835c744b1
otiza <93729229+otiza@users.noreply.github.com>
2022-10-30 02:32:35 +0100

    Add files via upload

---
+++ iterator_traits.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ITERATOR_TRAITS_HPP
+#define ITERATOR_TRAITS_HPP
+
+#include <iterator>
+template<class Iter>
+struct iterator_traits
+{
+    typedef typename Iter::difference_type difference_type;
+    typedef typename Iter::value_type value_type;
+    typedef typename Iter::pointer pointer;
+    typedef typename Iter::reference reference;
+    typedef typename Iter::iterator_category iterator_category;
+};
+template<class T>
+struct iterator_traits<T*>
+{
+    typedef  std::ptrdiff_t difference_type;
+    typedef  T value_type;
+    typedef  T* pointer;
+    typedef  T& reference;
+    typedef  std::random_access_iterator_tag iterator_category;
+};
+#endif
